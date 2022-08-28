3b297a9e14b5a106f2cc0be3dfc4fe3371132589
alex <alexzelinsky124@gmail.com>
2022-08-28 10:14:24 -0700

    modal context migrated to new repo

---
+++ react-app/src/context/Modal.js
@@ -0,0 +1,36 @@
+import React, { useContext, useRef, useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./Modal.css";
+
+const ModalContext = React.createContext();
+
+export function ModalProvider({ children }) {
+  const modalRef = useRef();
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    setValue(modalRef.current);
+  }, []);
+
+  return (
+    <>
+      <ModalContext.Provider value={value}>{children}</ModalContext.Provider>
+      {/* set the ref prop onto the div, so we can set value to be the modalRef.current in the useEffect */}
+      <div ref={modalRef} />
+    </>
+  );
+}
+
+export function Modal({ onClose, children }) {
+  const modalNode = useContext(ModalContext);
+  if (!modalNode) return null;
+
+  //   To get the elements to show up in the div in the modal provider, pass the rendered elements into createPortal's first argument
+  return ReactDOM.createPortal(
+    <div id="modal">
+      <div id="modal-background" onClick={onClose} />
+      <div id="modal-content">{children}</div>
+    </div>,
+    modalNode
+  );
+}
