df5060c98d3198d6f5dd4ff0d26610a586ed5491
alex <alexzelinsky124@gmail.com>
2022-08-28 17:17:30 -0700

    initial array problems seeded

---
+++ app/problems/array.py
@@ -0,0 +1,39 @@
+contains_duplicate = {
+    'name': 'Contains Duplicate',
+    'category': 'Array',
+    'description': 'Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.',
+    'example': '''Input: nums = [1,2,3,1]
+Output: true'''
+}
+
+squares_of_a_sorted_array = {
+    'name': 'Squares of a Sorted Array',
+    'category': 'Array',
+    'description': 'Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.',
+    'example': '''Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].'''
+}
+
+sort_colors = {
+    'name': 'Sort Colors',
+    'category': 'Array',
+    'description': """Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.""",
+    'example': '''Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]'''
+}
+
+product_of_array_except_self = {
+    'name': 'Product of Array Except Self',
+    'category': 'Array',
+    'description': """Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.""",
+    'example': '''Input: nums = [1,2,3,4]
+Output: [24,12,8,6]'''
+}
