296fed091253bb1df514dc6c54a178df2391471d
alex <alexzelinsky124@gmail.com>
2022-08-30 19:30:55 -0700

    seeded hash-map problems and ratings

---
+++ app/problems/hash_maps.py
@@ -0,0 +1,55 @@
+two_sum = {
+    'name': 'Two Sum',
+    'category': 'Hash Maps',
+    'description': """Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.""",
+    'example': """Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].""",
+    "difficulty": 'Easy'
+}
+
+majority_element = {
+    'name': 'Majority Element',
+    'category': 'Hash Maps',
+    'description': """Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.""",
+    'example': """Input: nums = [3,2,3]
+Output: 3""",
+    "difficulty": 'Easy'
+}
+
+valid_anagram = {
+    'name': 'Valid Anagram',
+    'category': 'Hash Maps',
+    'description': """Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.""",
+    'example': """Input: s = "anagram", t = "nagaram"
+Output: true""",
+    "difficulty": 'Easy'
+}
+
+group_anagrams = {
+    'name': 'Group Anagrams',
+    'category': 'Hash Maps',
+    'description': """Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.""",
+    'example': """Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]""",
+    "difficulty": 'Medium'
+}
+
+top_k_frequent_elements = {
+    'name': 'Top K Frequent Elements',
+    'category': 'Hash Maps',
+    'description': """Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.""",
+    'example': """Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]""",
+    "difficulty": 'Medium'
+}
--- app/seeds/ratings.py
+++ app/seeds/ratings.py
@@ -5,11 +5,21 @@ def seed_ratings():
     rating_2 = Rating(rating=5, problem_id=2, user_id=1)
     rating_3 = Rating(rating=5, problem_id=3, user_id=1)
     rating_4 = Rating(rating=5, problem_id=4, user_id=1)
+    rating_5 = Rating(rating=5, problem_id=5, user_id=1)
+    rating_6 = Rating(rating=5, problem_id=6, user_id=1)
+    rating_7 = Rating(rating=5, problem_id=7, user_id=1)
+    rating_8 = Rating(rating=5, problem_id=8, user_id=1)
+    rating_9 = Rating(rating=5, problem_id=9, user_id=1)
 
     db.session.add(rating_1)
     db.session.add(rating_2)
     db.session.add(rating_3)
     db.session.add(rating_4)
+    db.session.add(rating_5)
+    db.session.add(rating_6)
+    db.session.add(rating_7)
+    db.session.add(rating_8)
+    db.session.add(rating_9)
 
     db.session.commit()
 
